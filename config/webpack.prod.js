ff1580e971467c6aca6227c03eb252bd73a508e3
tomasy <solopea@gmail.com>
2018-12-07 21:16:01 +0800

    update config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -2,5 +2,6 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "production"
+  mode: "development",
+  devtool: "inline-source-map"
 });
