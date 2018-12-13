574cada629cb4dcce663625298cdae59766b1665
tomasy <solopea@gmail.com>
2018-12-13 16:07:55 +0800

    bugfix

--- src/newtab/index.tsx
+++ src/newtab/index.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import NewTab from './newtab';
 
-requestAnimationFrame(function() {
+document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(<NewTab />, document.getElementById('main'));
 });
