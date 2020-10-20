9aa3a7f6b62d0b9c37827b3868f2bf0d1da2a45e
tomasy <solopea@gmail.com>
2020-10-21 00:53:16 +0800

    feat: fullscreen

--- src/newtab/components/BottombarTools.tsx
+++ src/newtab/components/BottombarTools.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import { useContext, useCallback } from 'react';
-import { CommentOutlined, MacCommandOutlined } from '@ant-design/icons';
+import { CommentOutlined, FullscreenExitOutlined, FullscreenOutlined, MacCommandOutlined } from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../redux/reducers';
 import { SET_SHORTCUTS_MODAL_VISIBLE, UPDATE_ALLOW_COMMENT } from '../redux/actionTypes';
+import { useFullscreen } from 'ahooks';
 
 export default function BottombarTools() {
   const dispatch = useDispatch();
   const allowComment = useSelector((state: RootState) => state.comments.allowComment);
+  const [isFullscreen, { toggleFull }] = useFullscreen(document.documentElement);
   const onCommentBtnClick = useCallback(() => {
     dispatch({
       type: UPDATE_ALLOW_COMMENT,
@@ -25,11 +27,15 @@ export default function BottombarTools() {
     <div className="bottom-right-tools">
       <CommentOutlined
         style={{ color: '#fff', fontSize: '20px' }}
-        className={['icon-comment', allowComment ? 'icon-comment-enable' : 'icon-comment-disable'].join(' ')}
+        className={['icon icon-comment', allowComment ? 'icon-comment-enable' : 'icon-comment-disable'].join(' ')}
         onClick={onCommentBtnClick}
       />
-      <MacCommandOutlined style={{ color: '#ccc', fontSize: '18px' }} 
-        className="icon-shortcuts" onClick={onShortcutsBtnClick}/>
+      <MacCommandOutlined className="icon icon-shortcuts" onClick={onShortcutsBtnClick}/>
+      {
+        isFullscreen ?
+        <FullscreenExitOutlined className="icon icon-fullscreen" onClick={toggleFull}/> : 
+        <FullscreenOutlined className="icon icon-fullscreen" onClick={toggleFull}/>
+      }
     </div>
   );
 }
